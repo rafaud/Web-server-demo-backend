04f39b5c348a9093ad2176332f6f953c03be1cd3
rafaud <rafal.szadkowski@icloud.com>
2022-04-03 18:36:33 +0200

    fixed post mapping

--- src/main/kotlin/pl/edu/wat/szadkowski/rafal/web_server_demo/controller/PersonController.kt
+++ src/main/kotlin/pl/edu/wat/szadkowski/rafal/web_server_demo/controller/PersonController.kt
@@ -1,11 +1,7 @@
 package pl.edu.wat.szadkowski.rafal.web_server_demo.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import pl.edu.wat.szadkowski.rafal.web_server_demo.model.Person
-import pl.edu.wat.szadkowski.rafal.web_server_demo.model.enum.Sex
 import pl.edu.wat.szadkowski.rafal.web_server_demo.repository.PersonRepository
 
 @RestController
@@ -18,17 +14,8 @@ class PersonController (
     }
 
     @PostMapping("/person")
-    fun addPerson(@RequestParam person: Person): Person {
+    fun addPerson(@RequestBody person: Person): Person {
         person.id = null
         return personRepository.save(person)
-    }
-
-    @GetMapping("/person/add_random")
-    fun addRandomPerson(): Person {
-        return personRepository.save(
-            Person(name = "Karol",
-            surname = "Hermak",
-            sex = Sex.MALE)
-        )
     }
 }
